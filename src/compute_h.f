81b48235317a36a471ee59d560bb0df01f9f5a03
zhuwang46 <wangz1@uthscsa.edu>
2019-03-07 16:53:05 -0600

    fix a bug when calling a subroutine with n, should be 1 instead

--- src/compute_h.f
+++ src/compute_h.f
@@ -4,20 +4,20 @@ C output h
       subroutine compute_h(rfamily, n, y, fk_old, s, B, h)
               implicit none
               integer rfamily, i, n
-              double precision y(n), fk_old(n), s, B, u(n), g(n), h(n)
+              double precision y(n), fk_old(n), s, B, u(n), g, h(n)
 
       if(rfamily .EQ. 1)then
               do 10 i=1, n
               u(i)=y(i)-fk_old(i)
-              call gradient(rfamily, n, u(i), s, g(i))
-              h(i) = g(i)/B + fk_old(i)
+              call gradient(rfamily, 1, u(i), s, g)
+              h(i) = g/B + fk_old(i)
  10   continue
       else
          if(rfamily .EQ. 2 .OR. rfamily .EQ. 3 .OR. rfamily .EQ. 4)then
               do 30 i=1, n
               u(i)=y(i)*fk_old(i)
-              call gradient(rfamily, n, u(i), s, g(i))
-              h(i) = -y(i)*g(i)/B + fk_old(i)
+              call gradient(rfamily, 1, u(i), s, g)
+              h(i) = -y(i)*g/B + fk_old(i)
   30  continue
          endif
       endif
