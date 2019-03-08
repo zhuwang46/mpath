a893c1b851bce1cea38385cd688f542203deb747
zhuwang46 <wangz1@uthscsa.edu>
2019-03-08 17:41:34 -0600

    changes are needed due to Fortran subroutine changes

--- src/mpath_init.c
+++ src/mpath_init.c
@@ -21,14 +21,17 @@ extern void F77_NAME(compute_h)(void *, void *, void *, void *, void *, void *, 
 extern void F77_NAME(deveval)(void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(find_activeset)(void *, void *, void *, void *, void *);
 extern void F77_NAME(glmlink)(void *, void *, void *, void *, void *, void *);
-extern void F77_NAME(glmreg_fit_fortran)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(glmreg_fit_fortran)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(gradient)(void *, void *, void *, void *, void *);
 extern void F77_NAME(linkinv)(void *, void *, void *, void *);
 extern void F77_NAME(loglikfor)(void *, void *, void *, void *, void *, void *, void *);
-extern void F77_NAME(nclreg_fortran)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(loss)(void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(nclreg_fortran)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(nonconvexloss)(void *, void *, void *, void *, void *);
 extern void F77_NAME(outloop)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(outprod)(void *, void *, void *, void *, void *);
 extern void F77_NAME(penglm)(void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(pnorm_fortran)(void *);
 extern void F77_NAME(pred)(void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(zeval)(void *, void *, void *, void *, void *, void *, void *);
 
@@ -42,10 +45,13 @@ static const R_FortranMethodDef FortranEntries[] = {
 	    {"gradient",                   (DL_FUNC) &F77_NAME(gradient),            5},
 	    {"linkinv",                    (DL_FUNC) &F77_NAME(linkinv),             4},
 	    {"loglikfor",                  (DL_FUNC) &F77_NAME(loglikfor),           7},
-	    {"nclreg_fortran",             (DL_FUNC) &F77_NAME(nclreg_fortran),     32},
+	    {"loss",                       (DL_FUNC) &F77_NAME(loss),                7},
+	    {"nclreg_fortran",             (DL_FUNC) &F77_NAME(nclreg_fortran),     35},
+	    {"nonconvesloss",              (DL_FUNC) &F77_NAME(nonconvexloss),       5},
 	    {"outloop",                    (DL_FUNC) &F77_NAME(outloop),            35},
 	    {"outprod",                    (DL_FUNC) &F77_NAME(outprod),             5},
 	    {"penglm",                     (DL_FUNC) &F77_NAME(penglm),              7},
+	    {"pnorm_fortran",              (DL_FUNC) &F77_NAME(pnorm_fortran),       1},
 	    {"pred",                       (DL_FUNC) &F77_NAME(pred),                9},
 	    {"zeval",                      (DL_FUNC) &F77_NAME(zeval),               7},
 					        {NULL, NULL, 0}
