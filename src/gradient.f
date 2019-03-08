42bfb9b998be81f2986d0e4c6ee051e7792f5084
zhuwang46 <wangz1@uthscsa.edu>
2019-03-08 17:38:37 -0600

    change rfamilytype so no confusing when computing loss values and using other Fortran subroutines

--- src/gradient.f
+++ src/gradient.f
@@ -3,27 +3,27 @@ C converted from R package bst R/gradient function
 C input: family, u, s, no change
 C output: g
 C family: 
-C    clossR: 1, closs: 2, gloss: 3, qloss: 4
+C    clossR: 11, closs: 12, gloss: 13, qloss: 14
 C
       subroutine gradient(family, n, u, s, g)
       implicit none
       integer family, n, i
       double precision u(n), s, g(n), cval, pi
 
-      if(family .EQ. 1)then
+      if(family .EQ. 11)then
               do 10 i=1, n
               g(i) = u(i)/(s**2*dexp(u(i)**2/(2*s**2)))
 10    continue 
-      else if(family .EQ. 2)then
+      else if(family .EQ. 12)then
               cval = 1/(1 - dexp(-1/(2*s**2)))
               do 20 i=1, n
               g(i)=cval*(u(i)-1)/(s**2*dexp((1-u(i))**2/(2*s**2)))
 20    continue 
-      else if(family .EQ. 3)then
+      else if(family .EQ. 13)then
               do 30 i=1, n
               g(i)=-2**s*s*dexp(u(i))*(dexp(u(i))+1)**(-s-1)
 30    continue 
-      else if(family .EQ. 4)then
+      else if(family .EQ. 14)then
               pi = 3.14159
               do 40 i=1, n
               g(i) = -sqrt(2.0)/(dsqrt(pi)*s)*dexp(-u(i)**2/(2*s**2))
