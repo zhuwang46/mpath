84b3b550cb8c3b7c61fc9a63fb44283a132682ff
zhuwang46 <wangz1@uthscsa.edu>
2019-04-03 18:20:45 -0500

    Simplify computing
    Make it simpler
     Changes to be committed:
    	modified:   src/glmregnb_fortran.f

--- src/glmregnb_fortran.f
+++ src/glmregnb_fortran.f
@@ -11,16 +11,13 @@ C  vector of length nlambda
      +  maxit, rescale, standardize, iter, theta_est
       double precision x(n, m), y(n), lambda(nlambda), weights(n),
      +  offset(n), start(m+1), etastart(n), mustart(n), thetastart, 
-     +  del, epsbino, 
-     +  theta0(nlambda), d, tht(nlambda), eps, thresh, alpha, gam, t0,
-     +  eta(n), mu(n), penaltyfactor, beta_1(m), b0_1, 
-     +  beta(m, nlambda), b0(nlambda), yhat(n) 
+     +  del, epsbino, theta0(nlambda), d, tht(nlambda), eps, thresh, 
+     +  alpha, gam, t0, penaltyfactor, beta_1(m, 1), b0_1, 
+     +  beta(m, nlambda), b0(nlambda), yhat(n)
       
-      if(theta_est .EQ. 0)then
-          thetastart = theta0(1)
-      endif
       k = 1
 C      convout <- twologlik <- rep(NA, nlambda)
+      del = eps**0.25
 10    if(k <= nlambda)then
         if(trace .EQ.1 )then
             call intpr("loop in lambda:", -1,  k, 1)
@@ -30,14 +27,12 @@ C      convout <- twologlik <- rep(NA, nlambda)
         endif
         iter = 0
         d = 10
-C        Lm <- loglik(n, th, mu, Y, w)
-C        converged <- FALSE                                    
 30        if(d .GT. del .AND. iter <= maxit_theta)then
 	        call glmreg_fit_fortran(x, y, weights, n, m,start,
-     +       etastart, mustart, 
-     +       offset, 1, lambda(k), alpha, gam, rescale, 
-     +       standardize, penaltyfactor, thresh, epsbino, maxit,eps, 
-     +       thetastart, 4, penalty, trace, beta_1, b0_1, yhat)
+     +       etastart, mustart, offset, 1, lambda(k), alpha, gam, 
+     +       rescale, standardize, penaltyfactor, thresh, epsbino,
+     +       maxit,eps, thetastart, 4, penalty, trace, beta_1, 
+     +       b0_1, yhat)
             call dcopy(n, yhat, 1, mustart, 1)
             do i=1, n
             etastart(i) = dlog(mustart(i))
@@ -45,15 +40,13 @@ C        converged <- FALSE
             if(theta_est .EQ. 1)then
                 call theta_ml(y, mustart, n, weights, 10,
      +           eps**0.25, thetastart, trace)
-            else 
-                thetastart=theta0(k)
             endif
             d=0
             d=d+(start(1)-b0_1)**2
             start(1)=b0_1
             do j=1, m
-            d=d+(start(j+1)-beta_1(j))**2
-            start(j+1)=beta_1(j)
+            d=d+(start(j+1)-beta_1(j,1))**2
+            start(j+1)=beta_1(j,1)
             enddo
 C            del <- thetastart - th
 C            Lm0 <- Lm
@@ -71,17 +64,12 @@ C            }
             iter=iter + 1
             goto 30
         endif
-C        if(trace && iter > maxit.theta) {
-C            warning("alternation limit reached")
-C            fit$th.warn <- gettext("alternation limit reached")
-C        }
         tht(k) = thetastart
         call dcopy(m, beta_1, 1, beta(1:m, k), 1)
         b0(k) = b0_1
-C        Lm <- loglik(n, th, mu, Y, w)
-C        twologlik[k] <- as.vector(2 * Lm)
-C        nulldev[k] <- fit$nulldev
-C        resdev[k] <- fit$resdev
+C        call loglikFor(n, y, mustart, thetastart, weights, 4, loglik(k))
+C        nulldev(k) = nulldev0
+C        resdev(k) = resdev0
 C        if(trace) pll[,k] <- fit$pll
 C        convout[k] <- converged
 C        fitted[,k] <- fit$fitted.values
