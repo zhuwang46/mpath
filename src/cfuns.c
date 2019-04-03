bf7409cf572531dd4bd19219b77439432058100c
zhuwang46 <wangz1@uthscsa.edu>
2019-04-03 18:44:51 -0500

    Add new lines to call C functions
    Need to compute digamma and trigamma functions for family=negbin in
    Fortran.
    
     Changes to be committed:
    	modified:   src/cfuns.c

--- src/cfuns.c
+++ src/cfuns.c
@@ -3,3 +3,5 @@
 
 int F77_SUB(cisnan)(double *x) { return ISNAN(*x); }
 double F77_SUB(rlgamma)(double *x) { return lgammafn(*x); }
+double F77_SUB(rdigamma)(double *x) { return digamma(*x); }
+double F77_SUB(rtrigamma)(double *x) { return trigamma(*x); }
