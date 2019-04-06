6f0e6de4cc57a7cb9d22666067da33d7d7ed3fb2
zhuwang46 <wangz1@uthscsa.edu>
2019-04-06 11:10:41 -0500

    add needed files

--- src/mpath_init.c
+++ src/mpath_init.c
@@ -21,10 +21,13 @@ extern void F77_NAME(compute_h)(void *, void *, void *, void *, void *, void *, 
 extern void F77_NAME(copymatrix)(void *, void *, void *, void *);
 extern void F77_NAME(deveval)(void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(find_activeset)(void *, void *, void *, void *, void *);
+extern void F77_NAME(gfunc)(void *, void *, void *, void *, void *);
 extern void F77_NAME(glmlink)(void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(glmreg_fit_fortran)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(glmregnb_fortran)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(gradient)(void *, void *, void *, void *, void *);
 extern void F77_NAME(linkinv)(void *, void *, void *, void *);
+extern void F77_NAME(lmax_zipath)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void*);
 extern void F77_NAME(loglikfor)(void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(loss)(void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(nclreg_fortran)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
@@ -34,7 +37,9 @@ extern void F77_NAME(outprod)(void *, void *, void *, void *, void *);
 extern void F77_NAME(penglm)(void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(pnorm_fortran)(void *);
 extern void F77_NAME(pred)(void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(theta_ml)(void *, void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(zeval)(void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(zipath_fortran)(void *, void *, void *, void *, void *, void *, void*, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 
 static const R_FortranMethodDef FortranEntries[] = {
 	    {"checkconvergence",           (DL_FUNC) &F77_NAME(checkconvergence),    7},
@@ -42,10 +47,13 @@ static const R_FortranMethodDef FortranEntries[] = {
 	    {"copymatrix",                 (DL_FUNC) &F77_NAME(copymatrix),          4},
 	    {"deveval",                    (DL_FUNC) &F77_NAME(deveval),             7},
 	    {"find_activeset",             (DL_FUNC) &F77_NAME(find_activeset),      5},
+	    {"gfunc",                      (DL_FUNC) &F77_NAME(gfunc),               5},
 	    {"glmlink",                    (DL_FUNC) &F77_NAME(glmlink),             6},
 	    {"glmreg_fit_fortran",         (DL_FUNC) &F77_NAME(glmreg_fit_fortran), 27},
+	    {"glmregnb_fortran",           (DL_FUNC) &F77_NAME(glmregnb_fortran),   30},
 	    {"gradient",                   (DL_FUNC) &F77_NAME(gradient),            5},
 	    {"linkinv",                    (DL_FUNC) &F77_NAME(linkinv),             4},
+	    {"lmax_zipath",                (DL_FUNC) &F77_NAME(lmax_zipath),        18},
 	    {"loglikfor",                  (DL_FUNC) &F77_NAME(loglikfor),           7},
 	    {"loss",                       (DL_FUNC) &F77_NAME(loss),                7},
 	    {"nclreg_fortran",             (DL_FUNC) &F77_NAME(nclreg_fortran),     36},
@@ -55,7 +63,9 @@ static const R_FortranMethodDef FortranEntries[] = {
 	    {"penglm",                     (DL_FUNC) &F77_NAME(penglm),              7},
 	    {"pnorm_fortran",              (DL_FUNC) &F77_NAME(pnorm_fortran),       1},
 	    {"pred",                       (DL_FUNC) &F77_NAME(pred),                9},
+	    {"theta_ml",                   (DL_FUNC) &F77_NAME(theta_ml),            8},
 	    {"zeval",                      (DL_FUNC) &F77_NAME(zeval),               7},
+	    {"zipath_fortran",             (DL_FUNC) &F77_NAME(zipath_fortran),     44},
 					        {NULL, NULL, 0}
 };
 
