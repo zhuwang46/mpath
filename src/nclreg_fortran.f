8abef3c8668882b8f1ecba5754b3776008ad6e88
zhuwang46 <wangz1@uthscsa.edu>
2019-04-21 14:45:19 -0500

    All solution path change direction
    If the loss values change relatively too large from lambda A to B based on a pre-specified value epscycle, then recalculate the estimates for lambda A using the start values obtained from lambda B. This process continued for all the lambda values.
     Changes to be committed:
    	modified:   src/nclreg_fortran.f

--- src/nclreg_fortran.f
+++ src/nclreg_fortran.f
@@ -1,25 +1,25 @@
 C     used in nclreg.R
+C     output: nlambdacal: number of lambda actually computed including
+C     repeated ones
       subroutine nclreg_fortran(x, y, weights, n,m,start, etastart,
-     +     mustart, offset, nlambda, lambda, alpha, 
-     +     gam, standardize, penaltyfactor, maxit, eps, family,
-     +     penalty, trace, beta, b0, yhat, iter,
-     +     del, rfamily, B, s, los, pll, rescale, thresh, epsbino, 
-     +     theta, cost, active)
+     +     mustart, offset, iter, nlambda, lambda, alpha, gam, 
+     +     standardize, penaltyfactor, maxit, eps, epscycle, family,
+     +     penalty, trace, del,rfamily, B, s, rescale, thresh, cost, 
+     +     decreasing, active, beta, b0, yhat, los, pll, nlambdacal)
       implicit none
       integer n,m,i,ii,k,j,jj,penalty,nlambda,family,standardize, maxit,
      +     trace, iter, rfamily, rescale, jk, active, activeset(m), 
-     +     m_act, recycle,
+     +     m_act, nlambdacal, uturn, decreasing, cutpoint, 
      +     AllocateStatus, DeAllocateStatus, varsel(m), varsel_old(m)
       double precision x(n, m), y(n), weights(n),start(m+1),etastart(n),
      +     mustart(n), offset(n), lambda(nlambda), alpha, gam, eps, 
-     +     penaltyfactor(m), thresh, epsbino,  theta, beta(m, nlambda), 
+     +     penaltyfactor(m), thresh, beta(m, nlambda), epscycle,
      +     b0(nlambda), b0_1, yhat(n), d, del, lambda_i, fk_old(n), s, 
      +     B, h(n), fk(n), los(nlambda), pll(nlambda), cost, penval
       double precision, dimension(:, :), allocatable :: x_act
       double precision, dimension(:), allocatable :: start_act, beta_1,
      +     penaltyfactor_act
 
-      b0_1=0
       m_act = m
       jk = m
       allocate(start_act(m_act+1), stat=AllocateStatus)
@@ -47,7 +47,9 @@ C     used in nclreg.R
          varsel(j)=j
  101  continue
       i=1
-      recycle=0
+      nlambdacal=0
+      uturn=0
+      cutpoint=1
  10   if(i .LE. nlambda)then
          if(trace .EQ. 1)then
             call intpr("i=", -1, i, 1)
@@ -57,33 +59,22 @@ C     used in nclreg.R
      +        etastart, mustart, yhat, offset, lambda_i, alpha, gam, 
      +        penaltyfactor_act, maxit, eps, penalty, trace, iter,
      +        del, rfamily, B, s, thresh, beta_1, b0_1, fk)
+         nlambdacal=nlambdacal+1
          call loss(n, y, fk, cost, rfamily, s, los(i))
-C         if(i > 1)then
-C            if(abs(los(i)-los(i-1))/los(i) > 0.2)then
-C        redo (i-1)-lambda estimates with the current start_act for the
-C        i-th lambda. Note, i=i-2 not i-1 will do this since i=i+1 is
-C        computed before the end of the loop.
-C               i = i - 2
-C               recycle = 1
-C            endif
-C         endif
-C     penval = 0.d0
-         if(recycle==0)then
-            call penGLM(beta_1, m_act, lambda_i*penaltyfactor_act, 
-     +           alpha, gam, penalty, penval)
-            if(standardize .EQ. 1)then
-               pll(i)=los(i) + n*penval
-            else 
-               pll(i)=los(i) + penval
-            endif
-            b0(i) = b0_1
-            if(jk .GT. 0)then
-               do 200 ii = 1, m_act
-                  beta(varsel(ii), i) = beta_1(ii)
- 200           continue
-            endif
+         call penGLM(beta_1, m_act, lambda_i*penaltyfactor_act, 
+     +        alpha, gam, penalty, penval)
+         if(standardize .EQ. 1)then
+            pll(i)=los(i) + n*penval
+         else 
+            pll(i)=los(i) + penval
          endif
-         if(recycle==0 .AND. active .EQ. 1)then
+         b0(i) = b0_1
+         if(jk .GT. 0)then
+            do 200 ii = 1, m_act
+               beta(varsel(ii), i) = beta_1(ii)
+ 200        continue
+         endif
+         if(decreasing==0 .AND. uturn==0 .AND. active .EQ. 1)then
             call find_activeset(m_act, beta_1, eps, activeset, jk)
 C     this activeset is relative to the current x_act, but how about
 C     relative to x instead? compute varsel for true index in x
@@ -111,6 +102,26 @@ C     update x_act matrix
  55            continue
                m_act = jk
             endif
+         endif
+C     redo (i-1)-lambda estimates with the current start_act for the
+C     i-th lambda. Note, i=i-2 not i-1 will do this since i=i+1 is
+C     computed before the end of the loop.
+         if(decreasing==1)then
+            if(i > 1)then
+               if(abs(los(i)-los(i-1))/los(i) > epscycle)then
+                  if(cutpoint==1)then
+                     cutpoint = i
+                  endif
+                  i = i - 2
+                  uturn=1
+               else 
+                  uturn=0
+               endif
+            endif
+         endif
+         if(cutpoint > 1 .AND. uturn==0)then
+            i = cutpoint
+            cutpoint = 1
          endif
          i = i + 1
          goto 10
