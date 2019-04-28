ce8caee6eb943515b2c25a87d2cba1025e6bc66b
zhuwang46 <wangz1@uthscsa.edu>
2019-04-27 19:23:18 -0500

    Remove something just for debug purposes
    Remove something just for debug purposes
    
     Changes to be committed:
    	modified:   src/ziloss.f

--- src/ziloss.f
+++ src/ziloss.f
@@ -3,12 +3,11 @@ C     in package mpath/zipath.R
 C     family=3 - ziPoisson; 4 - ziNegBin
 C     output: los is the average loss
 
-      subroutine ziloss(n, y, weights, fc, fz, family, theta, los,
-     +       loglik0, loglik1)
+      subroutine ziloss(n, y, weights, fc, fz, family, theta, los)
       implicit none
       integer family, i, n
       double precision x, y(n), weights(n), fc(n), fz(n), mu, 
-     +     phi, theta, los, dpois, dnbinom, loglik0(n), loglik1(n)
+     +     phi, theta, los, dpois, dnbinom
       external dpois, dnbinom
 
       los = 0.d0
@@ -16,20 +15,6 @@ C     output: los is the average loss
          if(family .EQ. 3)then
             mu=dexp(fc(i))
             call linkinv(1, fz(i), 2, phi)
-            if(i==915)then
-                call intpr("i=", -1, i, 1)
-                call dblepr("y(i)=", -1, y(i), 1)
-                call dblepr("mu=", -1, mu, 1)
-                call dblepr("phi=", -1, phi, 1)
-                call dblepr("dlog(phi+dexp(dlog(1.0-phi)-mu))",
-     +          -1, dlog(phi+dexp(dlog(1.0-phi)-mu)), 1)
-                call dblepr("dpois(y(i), mu, 1)",
-     +               -1,dpois(int(y(i)),mu,1),1) 
-                call dblepr("dlog(1-phi)+dpois(y(i), mu, 1))", -1, 
-     +           dlog(1-phi)+dpois(int(y(i)), mu, 1), 1) 
-            endif
-               loglik0(i)=dlog(phi+dexp(dlog(1.0-phi)-mu))
-               loglik1(i)=dlog(1-phi)+dpois(int(y(i)), mu, 1)
             if(y(i) < 1)then
                los=los+weights(i)*dlog(phi+dexp(dlog(1.0-phi)-mu))
             else
