85dc7f131239dd8648fcdc3606d12466aaf1f625
zhuwang46 <zhuwang@gmail.com>
2020-10-19 13:57:21 -0500

    direct call glmnet::plotCoef

--- R/plot.glmreg.R
+++ R/plot.glmreg.R
@@ -1,5 +1,7 @@
 ### from glmnet version 1.9-1 package
 plot.glmreg=function(x, xvar=c("norm","lambda","dev"),label=FALSE,shade=TRUE, ...){
 xvar=match.arg(xvar)
- plotCoef(x$beta,lambda=x$lambda,df=x$df,dev=x$dev.ratio,label=label,xvar=xvar,...)
+which <- x$lambda
+if(length(which)>1) 
+eval(parse(text="glmnet:::plotCoef(x$beta,lambda=x$lambda,df=x$df,dev=x$dev.ratio,label=label,xvar=xvar,...)"))
 }
